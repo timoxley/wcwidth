174249cd10a7767bbe190e1f24085fd469ad8608
Tim Oxley <secoif@gmail.com>
2014-07-29 11:48:26 +0800

    Update readme/docs.

--- index.js
+++ index.js
@@ -19,22 +19,29 @@ module.exports.config = function(opts) {
   }
 }
 
-// The following functions define the column width of an ISO 10646 character as follows:
-//
-//    - The null character (U+0000) has a column width of 0.
-//    - Other C0/C1 control characters and DEL will lead to a return value of -1.
-//    - Non-spacing and enclosing combining characters (general category code Mn or Me in the
-//      Unicode database) have a column width of 0.
-//    - SOFT HYPHEN (U+00AD) has a column width of 1.
-//    - Other format characters (general category code Cf in the Unicode database) and ZERO WIDTH
-//      SPACE (U+200B) have a column width of 0.
-//    - Hangul Jamo medial vowels and final consonants (U+1160-U+11FF) have a column width of 0.
-//    - Spacing characters in the East Asian Wide (W) or East Asian Full-width (F) category as
-//      defined in Unicode Technical Report #11 have a column width of 2.
-//    - All remaining characters (including all printable ISO 8859-1 and WGL4 characters, Unicode
-//      control characters, etc.) have a column width of 1.
-//
-// This implementation assumes that characters are encoded in ISO 10646.
+/*
+ *  The following functions define the column width of an ISO 10646
+ *  character as follows:
+ *  - The null character (U+0000) has a column width of 0.
+ *  - Other C0/C1 control characters and DEL will lead to a return value
+ *    of -1.
+ *  - Non-spacing and enclosing combining characters (general category
+ *    code Mn or Me in the
+ *    Unicode database) have a column width of 0.
+ *  - SOFT HYPHEN (U+00AD) has a column width of 1.
+ *  - Other format characters (general category code Cf in the Unicode
+ *    database) and ZERO WIDTH
+ *    SPACE (U+200B) have a column width of 0.
+ *  - Hangul Jamo medial vowels and final consonants (U+1160-U+11FF)
+ *    have a column width of 0.
+ *  - Spacing characters in the East Asian Wide (W) or East Asian
+ *    Full-width (F) category as
+ *    defined in Unicode Technical Report #11 have a column width of 2.
+ *  - All remaining characters (including all printable ISO 8859-1 and
+ *    WGL4 characters, Unicode control characters, etc.) have a column
+ *    width of 1.
+ *  This implementation assumes that characters are encoded in ISO 10646.
+*/
 
 function wcswidth(str, opts) {
   if (typeof str !== 'string') return wcwidth(str, opts)
