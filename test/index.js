295c8a40c7cadd5fb3a3b6b24de4c6e8b86772cc
Tim Oxley <secoif@gmail.com>
2014-07-29 11:48:38 +0800

    Use tape vs mocha.

--- test/index.js
+++ test/index.js
@@ -1,54 +1,64 @@
+"use strict"
+
 var wcwidth = require('../')
-var assert = require('assert')
+var test = require('tape')
 
-describe('wcwidth', function() {
-  it('handles regular strings', function() {
-    assert.strictEqual(wcwidth('abc'), 3)
-  })
+test('handles regular strings', function(t) {
+  t.strictEqual(wcwidth('abc'), 3)
+  t.end()
+})
 
-  it('handles multibyte strings', function() {
-    assert.strictEqual(wcwidth('字的模块'), 8)
-  })
+test('handles multibyte strings', function(t) {
+  t.strictEqual(wcwidth('字的模块'), 8)
+  t.end()
+})
 
-  it('handles multibyte characters mixed with regular characters', function() {
-    assert.strictEqual(wcwidth('abc 字的模块'), 12)
-  })
+test('handles multibyte characters mixed with regular characters', function(t) {
+  t.strictEqual(wcwidth('abc 字的模块'), 12)
+  t.end()
+})
 
-  it('ignores control characters e.g. \\n', function() {
-    assert.strictEqual(wcwidth('abc\n字的模块\ndef'), 14)
-  })
+test('ignores control characters e.g. \\n', function(t) {
+  t.strictEqual(wcwidth('abc\n字的模块\ndef'), 14)
+  t.end()
+})
 
-  it('ignores bad input', function() {
-    assert.strictEqual(wcwidth(''), 0)
-    assert.strictEqual(wcwidth(3), 0)
-    assert.strictEqual(wcwidth({}), 0)
-    assert.strictEqual(wcwidth([]), 0)
-    assert.strictEqual(wcwidth(), 0)
-  })
+test('ignores bad input', function(t) {
+  t.strictEqual(wcwidth(''), 0)
+  t.strictEqual(wcwidth(3), 0)
+  t.strictEqual(wcwidth({}), 0)
+  t.strictEqual(wcwidth([]), 0)
+  t.strictEqual(wcwidth(), 0)
+  t.end()
+})
 
-  it('ignores nul (charcode 0)', function() {
-    assert.strictEqual(wcwidth(String.fromCharCode(0)), 0)
-  })
+test('ignores nul (charcode 0)', function(t) {
+  t.strictEqual(wcwidth(String.fromCharCode(0)), 0)
+  t.end()
+})
 
-  it('ignores nul mixed with chars', function() {
-    assert.strictEqual(wcwidth('a' + String.fromCharCode(0) + '\n字的'), 5)
-  })
+test('ignores nul mixed with chars', function(t) {
+  t.strictEqual(wcwidth('a' + String.fromCharCode(0) + '\n字的'), 5)
+  t.end()
+})
 
-  it('can have custom value for nul', function() {
-    assert.strictEqual(wcwidth.config({
-      nul: 10
-    })(String.fromCharCode(0) + 'a字的'), 15)
-  })
+test('can have custom value for nul', function(t) {
+  t.strictEqual(wcwidth.config({
+    nul: 10
+  })(String.fromCharCode(0) + 'a字的'), 15)
+  t.end()
+})
 
-  it('can have custom control char value', function() {
-    assert.strictEqual(wcwidth.config({
-      control: 1
-    })('abc\n字的模块\ndef'), 16)
-  })
+test('can have custom control char value', function(t) {
+  t.strictEqual(wcwidth.config({
+    control: 1
+  })('abc\n字的模块\ndef'), 16)
+  t.end()
+})
 
-  it('negative custom control chars == -1', function() {
-    assert.strictEqual(wcwidth.config({
-      control: -1
-    })('abc\n字的模块\ndef'), -1)
-  })
+test('negative custom control chars == -1', function(t) {
+  t.strictEqual(wcwidth.config({
+    control: -1
+  })('abc\n字的模块\ndef'), -1)
+  t.end()
 })
